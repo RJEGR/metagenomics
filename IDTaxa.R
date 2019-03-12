47d030ebde52a78728321c8cc923921c1cf09a86
cicese <cigom@ciceses-MacBook-Pro.local>
2019-03-12 14:12:29 -0700

    decipher tested
    ok

---
+++ IDTaxa.R
@@ -0,0 +1,232 @@
+
+# Author: Ricardo Gomez-Reyes | modified from: Erik S. Wright (2018) - Classify Sequences
+
+# The classification process is split into two parts: 
+# training carried out by:
+# LearnTaxa and testing with IdTaxa
+# How to use: Rscript --vanilla IDTaxa.R input[string] reference[string] taxonomy[string] threshold[interger - by default 99]
+# reference are fasta format of sequence references 
+# taxonomy are assignments for sequences embed in the reference file; two columns as in rdp-mothur format (';' separated ranks-by-rank)
+## >> Note: All elements of taxonomy must contain 'Root;'
+
+# Rscript --vanilla IDTaxa.R run012_relax_ASVs.P68.worms.fasta BOLD_public_species.fasta BOLD_public_species.tax.Root 80
+## Clean workspace
+rm(list=ls()); 
+
+# Path ----
+library(DECIPHER)
+
+path = '/Users/cigom/metagenomics/COI/run012'
+setwd(path)
+
+##  args insertion ----
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<3) {
+  stop("!!!\n. 
+       PLEASE, INPUT NECESSARY FILES IN THE SYNTAXIS AS FOLLOW EXAMPLE:\n
+       Rscript --vanilla IDTaxa.R input[string] reference[string] taxonomy[string] threshold[interger] .\n", call.=FALSE)
+} else { 
+  query <- args[1]
+  seqs_name <- args[2]
+  tax_name <- args[3] }
+
+# Read inputs ----
+
+# query <- "run012_relax_ASVs.P68.worms.fasta"
+q_path <- paste0(path, "/", query)
+
+# Sequnces references path
+# seqs_name <- "IctiosConsenso.fasta"
+# seqs_name <- 'BOLD_public_species.fasta'
+seqs_path <- paste0(path, "/", seqs_name)
+
+# taxonomy assigments path
+# tax_name <- 'IctiosConsenso.tax'
+# tax_name <- 'BOLD_public_species.tax.Root'
+tax_path <- paste0(path, "/", tax_name)
+
+#
+tag <- strsplit(query, "[.]")[[1]]
+tag_ref <- strsplit(seqs_name, "[.]")[[1]]
+out_name <- paste0(paste(tag[-length(tag)], collapse="."), "_",
+                   paste(tag_ref[-length(tag_ref)], collapse="."), "_",
+                                                  "DECIPHER.taxonomy")
+#
+# Parameters ----
+# 
+if (length(args) > 3) { threshold <- as.numeric(args[4]) } else { threshold <- 99 }
+
+
+## Training parameters
+
+maxGroupSize <- 1 # max sequences per label (>= 1); can be set to Inf (infinity) to allow for an unlimited number of sequences per group.
+maxIterations <- 1 # must be >= 1; also can remove mislabeled in the training data by turning itinerations > 1 
+allowGroupRemoval <- FALSE # if maxIterations > 1 set allowGroupRemoval <- TRUE
+
+
+#
+# Load sequence from reference data-base: ----
+#
+# Read sequences into memory
+seqs <- readDNAStringSet(seqs_path) # or  readRNAStringSet for RNA
+# if gaps in sequences:
+seqs <- RemoveGaps(seqs)
+
+#
+# Read the taxonomic assigments from data-base ----
+# 
+taxonomy <- read.table(tax_path, header = FALSE, stringsAsFactors = FALSE)
+
+# Renombramos el nombre de secuencias con la asignacion correspondiente:
+if (identical(taxonomy$V1, names(seqs))) {
+               names(seqs) <- taxonomy$V2
+               groups <- names(seqs)
+               groups <- gsub("(.*)(Root;)", "\\2", groups)
+               groupCounts <- table(groups)
+               cat("number of groups in reference are:\n", length(u_groups <- names(groupCounts)))
+    } else {
+               groups <- taxonomy$V2
+               groups <- gsub("(.*)(Root;)", "\\2", groups)
+               groupCounts <- table(groups)
+               cat("number of groups in reference are:\n", length(u_groups <- names(groupCounts)))
+               }
+                 
+#
+# Process the training set of data-base: ----
+#
+
+# Pruning the traning set
+# Count the number of representative per group
+
+# maxGroupSize <- 2 # max sequences per label (>= 1); can be set to Inf (infinity) to allow for an unlimited number of sequences per group.
+remove <- logical(length(seqs))
+
+for (i in which(groupCounts > maxGroupSize)) {
+  index <- which(groups==u_groups[i])
+  keep <- sample(length(index),
+                 maxGroupSize)
+  remove[index[-keep]] <- TRUE
+}
+
+sum(remove) # number of sequences eliminated
+
+# Iteratively training the classifier
+# Is it will identify any training sequences whose 
+# assigned classifications completely (with very high confidence) disagree with their predicted classification.
+
+# maxIterations <- 1 # must be >= 1; also can remove mislabeled in the training data by turning itinerations > 1 
+# allowGroupRemoval <- FALSE
+
+probSeqsPrev <- integer()
+taxid <- NULL
+
+for (i in seq_len(maxIterations)) {
+  cat("Training iteration: ", i, "\n", sep="")
+  # train the classifier
+  trainingSet <- LearnTaxa(seqs[!remove],
+                           names(seqs)[!remove],
+                           taxid)
+  # look for problem sequences
+  probSeqs <- trainingSet$problemSequences$Index
+  if (length(probSeqs)==0) {
+    cat("No problem sequences remaining.\n")
+    break
+  } else if (length(probSeqs)==length(probSeqsPrev) &&
+             all(probSeqsPrev==probSeqs)) {
+    cat("Iterations converged.\n")
+    break
+  }
+  if (i==maxIterations)
+    break
+  probSeqsPrev <- probSeqs
+  # remove any problem sequences
+  index <- which(!remove)[probSeqs]
+  remove[index] <- TRUE # remove all problem sequences
+  if (!allowGroupRemoval) {
+    # replace any removed groups
+    missing <- !(u_groups %in% groups[!remove])
+    missing <- u_groups[missing]
+    if (length(missing) > 0) {
+      index <- index[groups[index] %in% missing]
+      remove[index] <- FALSE # don't remove
+    }
+  }
+}
+
+
+# View training results:
+
+trainingSet
+plot(trainingSet)
+
+#
+# Classifying Sequences: ----
+# 
+
+q_seqs <- readDNAStringSet(q_path)
+q_seqs <- RemoveGaps(q_seqs) # if gaps remove it
+
+ids <- IdTaxa(q_seqs, 
+              trainingSet, 
+              type="extended",
+              strand = "top",
+              threshold = threshold,
+              processors = 1)
+
+#ids[1:5]
+#ids[[2]]
+#ids[c(10, 25)]
+#c(ids[10], ids[25])
+
+plot(ids, trainingSet)
+
+# subset any rank
+rank <- sapply(ids,
+                 function(x) {
+                   w <- which(x$rank=="Root")
+                   if (length(w) != 1) {
+                     "unknown"
+                   } else {
+                     x$taxon[w]
+                   }
+                 })
+# table(rank)
+
+taxon <- sapply(ids,
+                  function(x)
+                    x$taxon[length(x$taxon)])
+
+# Output assignments: ----
+output <- data.frame(sapply(ids,
+                     function(x)
+                       paste(x$taxon,
+                             collapse=";")))
+
+
+output <- data.frame(ASV = rownames(output), Taxonomy = output[,1])
+write.table(output, paste0(path, '/', out_name), sep = "\t", row.names = FALSE, col.names = TRUE)
+      
+
+#
+output_confidence <- sapply(ids,
+                 function (id) {
+                   paste(id$taxon,
+                         " (",
+                         round(id$confidence, digits=1),
+                         "%)",
+                         sep="",
+                         collapse="; ")})
+
+
+writeLines(output_confidence, paste0(path, '/', out_name, ".confidence"))
+
+
+quit(save='no')
+
+
+summary(confidence <- sapply(ids,
+                             function (id) {
+                               round(id$confidence, digits=1)
+                             }))
--- species_resolution_per_db.R
+++ species_resolution_per_db.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
 # Load libraries ----
-.cran_packages <- c("stringr", "tidyverse", "reshape2", "ggplot2") 
-
+.cran_packages <- c("stringr", "reshape2") 
+# .cran_packages <- c("stringr", "tidyverse", "reshape2", "ggplot2") 
 
 # 1. cran_package
 .inst <- .cran_packages %in% installed.packages()
@@ -30,40 +30,10 @@ source(file = "~/Documents/GitHub/metagenomics/converted2worms/cigom_load_dada2_
 
 ## Load assignation result ----
 
-"read.wangtax" <- function(x) {
-    
-    rdp_df <- NULL
-# wang.taxonomy, threshold = 99
-    rdp_df <- load.mothurdp_boots(x, 80)  #output is data.frame 'rdp_df'
-
-# Remove unclassified and replace underscores by spaces
-    rdp_df <- mutate_all(data.frame(rdp_df), funs(str_replace_all(., c(".+_unclassified"=NA, "_"=" "))))
-
-    rdp_df$ASV <- str_replace(rdp_df$ASV, " ", "_")
-
-    max.rank <-  ncol(rdp_df) -1 
-
-# Tag starting level (SL) (aka calculate resolution)
-    rdp_df$SL <- max.rank
-
-    for(t in max.rank:2){
-       rr <- t-1  #rank real 
-       rc <- t+1  #column-index (rank) para corregir
-            if(t == max.rank){
-               rdp_df$SL[is.na(rdp_df[,rc])] <- rr
-                } else {
-                rdp_df$SL[is.na(rdp_df[,rc]) & rdp_df$SL <= t ] <- rr
-        }
-    }
-
-return(rdp_df) 
-}
-
-
 bold <- read.wangtax(bold.file)
 midori <- read.wangtax(midori.file)
 
-if (identical(names(bold), names(midori))) { TL <- names(bold) 
+if (identical(names(bold), names(midori))) { TL <- names(bold)[-c(1,ncol(bold))]
     } # else {TL <- rank}
 
 # Add column for converted level (CL)
@@ -75,7 +45,73 @@ n_undetermined_midori <- nrow(midori[midori$Phylum == 'Undetermined',])
 
 # Plots ----
 
-# BOLD_public_species
+
+x <- data.frame(Rank=TL[-1],
+            midori=data.frame(table(midori$SL))[,2],
+            bold=data.frame(table(bold$SL))[,2])
+
+midori_ <- NULL
+for (i in 1:nrow(midori)) {
+    rl <- midori$SL[i] + 1
+    midori_[[i]] <- list(rank=names(x)[rl], linage=x[i,rl])
+    
+}
+
+bold_ <-NULL
+for (i in 1:nrow(bold)) {
+    rl <- bold$SL[i] + 1
+    bold_[[i]] <- list(rank=names(x)[rl], linage=x[i,rl])
+}
+
+
+
+midori_ <- do.call(rbind, midori_)
+bold_ <- do.call(rbind, bold_)
+
+data <- data.frame( ASV = midori$ASV,
+                    midori = do.call(rbind, midori_[,1]), 
+                    lineage_x = do.call(rbind, midori_[,2]),
+                    bold = do.call(rbind, bold_[,1]),
+                    lineage_y = do.call(rbind, bold_[,2]))
+
+
+
+data %>% 
+  distinct(lineage_x, lineage_y) ->  lineages 
+
+data[,c(2,3)] %>% group_by(midori) %>%
+  summarise(n = table(lineage_x)) -> test
+
+
+data_tidy <- melt(data, id.vars = c("ASV", "lineage_x", "lineage_y"),
+                    variable.name = "DB",
+                    value.name = "Rank")
+
+# or
+
+data_tidy <- melt(data[-1], id.vars=c("midori", "bold"))
+
+# aggregate(data_tidy[,'DB'], by=list(data_tidy[,'Rank']), FUN = table)
+
+data_agg <- aggregate(data_tidy[,c('lineage_x', 'lineage_y')], by=list(data_tidy[,'Rank']), FUN = table)
+data_agg <- data.frame(level = SL_diff[,1], SL_diff[,2])
+# bastante complicado elaborar el formato del sanky pplot
+
+L_tidy$Rank <- factor(L_tidy$Rank, levels = TL[2:7], ordered = T)
+
+testvis <- data_tidy[sample(nrow(data_tidy), 100), ]
+
+ggplot(data = x,
+       aes(axis1 = midori, axis2 = bold)) +
+  geom_alluvium(aes(fill = Rank)) + #aes(fill = DB)
+  geom_stratum() + 
+  geom_text(stat = "stratum", label.strata = TRUE) +
+  theme_minimal() +
+  ggtitle("")
+
+
+
+# BOLD_public_species table(bold$bold_SL)
 #   2    3    4    5    6    7 
 # 8567  176  198  322  352 4257 
 # Removing 7937 Undetermined Phylum from the dataset
@@ -87,7 +123,7 @@ n_undetermined_midori <- nrow(midori[midori$Phylum == 'Undetermined',])
 # make function to read an process by wangtax
 
 # Lineplot. Distribucion de las asignaciones entre bases de datos
-L_tables <- data.frame(Rank=TL[3:8], Level=2:7, 
+L_tables <- data.frame(Rank=TL[2:7], Level=2:7, 
                        bold=data.frame(table(bold$SL))[,'Freq'], 
                        midori=data.frame(table(midori$SL))[, 'Freq'])
 
@@ -97,7 +133,7 @@ L_tables$diff <- abs(L_tables$bold - L_tables$midori)
 L_tidy <- melt(L_tables, id.vars=c("Rank", "Level", "diff"),
                 variable.name = "DB",
                 value.name = "ASVs")
-L_tidy$Rank <- factor(L_tidy$Rank, levels = TL[3:8], ordered = T)
+L_tidy$Rank <- factor(L_tidy$Rank, levels = TL[2:7], ordered = T)
 
 
 # or with tidy
@@ -157,7 +193,7 @@ SL_tidy <- melt(SL_diff, id.vars="level",
                          value.name = "ASVs")
 
 SL_tidy$level <- abs(SL_tidy$level)
-# SL_tidy$Rank <- factor(SL_tidy$rank, levels= TL[3:8], labels= TL[3:8], ordered = T)
+# SL_tidy$Rank <- factor(SL_tidy$rank, levels= TL[2:7], labels= TL[2:7], ordered = T)
 
 datavis = SL_tidy[SL_tidy$DB != 'equal' & SL_tidy$ASVs !=0,]
 no_change = SL_tidy[SL_tidy$DB == 'equal' & SL_tidy$ASVs !=0, 'ASVs']
@@ -179,20 +215,124 @@ ggplot(datavis, aes(level, ASVs, fill = DB)) +
 
 quit(save = 'no')
 
+# Basicamente se necesita para cada ASV la taxonomia (una columna por nivel) con DB_1 y con DB_2
+
+names(midori)[-1] <- paste("midori", names(midori)[-1], sep="_")
+names(bold)[-1] <- paste("bold", names(bold)[-1], sep="_")
+
+input_midori_bold <- cbind(midori, bold[,-1])
+
+# aniadir una columna, rango 1
+input_midori_bold$midori_vs_bold <- 1
+# recorrer hacia el 7 en loop
+for(t in 1:7){
+    # Generar nombre de la columna con taxonomia por morfologia
+    midori_rank <- paste("midori", TL[t], sep="_")
+    # Generar nombre de la columna con taxonomica por molecular
+    bold_rank <- paste("bold", TL[t], sep="_")
+    # Aquellos renglones donde coincidan los nombres tienen LCR = t
+    input_midori_bold$midori_vs_bold[ which(input_midori_bold[,midori_rank] == input_midori_bold[,bold_rank]) ] <- t
+}
+
+# t(input_midori_bold[11,])
+# paste("midori", TL[-1], sep="_")
+
+
+
+input_midori_bold_m <- melt(input_midori_bold, id.vars=c("ASV", "midori_SL", "bold_SL", "midori_vs_bold"),
+                variable.name = "Rank",
+                value.name = "linage")
+
+input_midori_bold_m$DB <- sapply(strsplit(as.vector(input_midori_bold_m$Rank), "_"), "[[", 1)
+input_midori_bold_m$Rank <- sapply(strsplit(as.vector(input_midori_bold_m$Rank), "_"), "[[", 2)
+
+
+sankey_data <- input_midori_bold_m[sample(nrow(input_midori_bold_m), 100), ] # to test
+
+sankey_data %>% group_by(Rank, linage, DB) %>%
+  summarise(n = sum(midori_vs_bold)) -> sankey_vis
+
+ggplot(data = sankey_vis,
+       aes(axis1 = Rank, axis2 = linage,
+           y = n)) +
+  #scale_x_discrete(limits = c("Class", "Sex", "Age"), expand = c(.1, .05)) +
+ # xlab("Demographic") +
+  geom_alluvium(aes(fill = DB)) +
+  geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
+  theme_minimal() +
+  ggtitle("")
 
 
 
 
- Bold  equal Midori 
-   577  12795    500 
 
-   Es decir, hay una diferencia de 500/577 mejor asignaciones entre midori y bold, mientras que 12,795 ASVs
-   alcanzaron la misma resolucion usando ambas bases de referencia
 
-        "bold"   "midori" "diff"   "Reward"
-ASV_13745    7      2   -5   Bold
-ASV_13778    7      6   -1   Bold
-ASV_13852    2      2    0  equal
-ASV_13853    3      6    3 Midori
-ASV_13854    2      7    5 Midori
+# Percentage of correlacion entre midori y bold
+midori_bold_corr <- 100-round((table(input_midori_bold$midori_vs_bold)/nrow(input_midori_bold))*100, 2)
+midori_bold_corr
 
+# match_rank / n_asvs * 100
+
+cor <- input_midori_bold[, c("midori_SL", "bold_SL", "midori_vs_bold")]
+midori_vs_bold <- input_midori_bold[input_midori_bold$midori_vs_bold == 7, c('bold_Species', 'midori_Species')]
+
+
+ggplot(table_m, 
+       aes(x=item_morfo, y=rank_txt, shape=resolution_type, size=resolution_type, colour=resolution_type)) +
+    geom_point()
+
+table_m[which(table_m$Query_ID %in% "ConcensoIctio25"), ]
+
+
+#          Query_ID                              item_morfo morfo_Order resolution_type rank_txt            solo_morfo
+# 1  ConcensoIctio25 ConcensoIctio25\nVinciguerria poweriae      Stomii       Molecular    Genus Vinciguerria poweriae
+# 34 ConcensoIctio25 ConcensoIctio25\nVinciguerria poweriae      Stomii   Morphological  Species Vinciguerria poweriae
+# 67 ConcensoIctio25 ConcensoIctio25\nVinciguerria poweriae      Stomii   Matching rank    Genus Vinciguerria poweriae
+
+# Plot
+poster_plot <- ggplot(table_m, 
+       aes(x=item_morfo, y=rank_txt, shape=resolution_type, size=resolution_type, colour=resolution_type)) +
+    geom_point() +
+    facet_grid(morfo_Order ~., scales="free", space="free") +
+    labs(title="", x="", y="") +
+    scale_shape_manual(name="Identification approach", values=c(16, 1, 2)) +
+    scale_colour_manual(name="Identification approach", values=color_asign) +
+    scale_size_manual(values=c(6,7.2,7.5)) +
+    guides(size=F, colour = guide_legend(override.aes = list(size=6))) +
+    coord_flip() +
+    theme_light() +
+    theme(axis.text.x = element_text(angle=0, size=12, hjust=0.5, vjust=0.5),
+          axis.text.y = element_text(size=12),
+          strip.text.y = element_text(angle=0, size=12),
+          #strip.background = element_rect(colour="grey40", fill=NA),
+          legend.text = element_text(size=12),
+          legend.title = element_text(size=12, face="bold"),
+          legend.key = element_rect(fill="white"),
+          legend.position = "top")
+poster_plot  
+
+
+#alluvial
+# devtools::install_github("r-lib/rlang", build_vignettes = TRUE)
+detach("package:tidyverse", unload=TRUE)
+library(ggalluvial)
+titanic_wide <- data.frame(Titanic)
+head(titanic_wide)
+
+ggplot(data = titanic_wide,
+       aes(axis1 = Class, axis2 = Sex, axis3 = Age,
+           y = Freq)) +
+  scale_x_discrete(limits = c("Class", "Sex", "Age"), expand = c(.1, .05)) +
+  xlab("Demographic") +
+  geom_alluvium(aes(fill = Survived)) +
+  geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
+  theme_minimal() +
+  ggtitle("passengers on the maiden voyage of the Titanic",
+          "stratified by demographics and survival")
+
+
+head(sp <- data.frame(table(bold[!is.na(bold$bold_Species) ,8])))
+
+sp[c(grep(' sp[.]', sp[,1])),]
+
+input_midori_bold_m[input_midori_bold_m$Rank == 'Species' & input_midori_bold_m$]
