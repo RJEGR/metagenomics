3519156a52150ab71ea3ce32abf2f9572511b232
cicese <cigom@ciceses-MacBook-Pro.local>
2019-10-01 15:25:58 -0700

    add ggplot require

--- plotQualityProfile.R
+++ plotQualityProfile.R
@@ -1,5 +1,7 @@
 plotQP <- function (fl, n = 5e+05, aggregate = FALSE) 
 {
+  .packages <- c("ggplot2")
+  sapply(.packages, require, character.only = TRUE)
   statdf <- data.frame(Cycle = integer(0), Mean = numeric(0), 
                        Q25 = numeric(0), Q50 = numeric(0), Q75 = numeric(0), 
                        Cum = numeric(0), file = character(0))
@@ -112,4 +114,81 @@ plotQP <- function (fl, n = 5e+05, aggregate = FALSE)
     }
   }
   p
+}
+
+
+# Get quality tbl -----
+
+tblQP <- function (fl, n = 5e+05, aggregate = FALSE) 
+{
+  statdf <- data.frame(Cycle = integer(0), Mean = numeric(0), 
+                       Q25 = numeric(0), Q50 = numeric(0), Q75 = numeric(0), 
+                       Cum = numeric(0), file = character(0))
+  anndf <- data.frame(minScore = numeric(0), label = character(0), 
+                      rclabel = character(0), rc = numeric(0), file = character(0))
+  FIRST <- TRUE
+  for (f in fl[!is.na(fl)]) {
+    srqa <- ShortRead::qa(f, n = n)
+    df <- srqa[["perCycle"]]$quality
+    rc <- sum(srqa[["readCounts"]]$read)
+    if (rc >= n) {
+      rclabel <- paste("Reads >= ", n)
+    }
+    else {
+      rclabel <- paste("Reads: ", rc)
+    }
+    means <- rowsum(df$Score * df$Count, df$Cycle)/rowsum(df$Count, 
+                                                          df$Cycle)
+    get_quant <- function(xx, yy, q) {
+      xx[which(cumsum(yy)/sum(yy) >= q)][[1]]
+    }
+    q25s <- by(df, df$Cycle, function(foo) get_quant(foo$Score, 
+                                                     foo$Count, 0.25), simplify = TRUE)
+    q50s <- by(df, df$Cycle, function(foo) get_quant(foo$Score, 
+                                                     foo$Count, 0.5), simplify = TRUE)
+    q75s <- by(df, df$Cycle, function(foo) get_quant(foo$Score, 
+                                                     foo$Count, 0.75), simplify = TRUE)
+    cums <- by(df, df$Cycle, function(foo) sum(foo$Count), 
+               simplify = TRUE)
+    if (!all(sapply(list(names(q25s), names(q50s), names(q75s), 
+                         names(cums)), identical, rownames(means)))) {
+      stop("Calculated quantiles/means weren't compatible.")
+    }
+    if (FIRST) {
+      plotdf <- cbind(df, file = basename(f))
+      FIRST <- FALSE
+    }
+    else {
+      plotdf <- rbind(plotdf, cbind(df, file = basename(f)))
+    }
+    statdf <- rbind(statdf, data.frame(Cycle = as.integer(rownames(means)), 
+                                       Mean = means, Q25 = as.vector(q25s), Q50 = as.vector(q50s), 
+                                       Q75 = as.vector(q75s), Cum = 10 * as.vector(cums)/rc, 
+                                       file = basename(f)))
+    anndf <- rbind(anndf, data.frame(minScore = min(df$Score), 
+                                     label = basename(f), rclabel = rclabel, rc = rc, 
+                                     file = basename(f)))
+  }
+  anndf$minScore <- min(anndf$minScore)
+  if (aggregate) {
+    plotdf.summary <- aggregate(Count ~ Cycle + Score, plotdf, 
+                                sum)
+    plotdf.summary$label <- paste(nrow(anndf), "files (aggregated)")
+    means <- rowsum(plotdf.summary$Score * plotdf.summary$Count, 
+                    plotdf.summary$Cycle)/rowsum(plotdf.summary$Count, 
+                                                 plotdf.summary$Cycle)
+    q25s <- by(plotdf.summary, plotdf.summary$Cycle, function(foo) get_quant(foo$Score, 
+                                                                             foo$Count, 0.25), simplify = TRUE)
+    q50s <- by(plotdf.summary, plotdf.summary$Cycle, function(foo) get_quant(foo$Score, 
+                                                                             foo$Count, 0.5), simplify = TRUE)
+    q75s <- by(plotdf.summary, plotdf.summary$Cycle, function(foo) get_quant(foo$Score, 
+                                                                             foo$Count, 0.75), simplify = TRUE)
+    cums <- by(plotdf.summary, plotdf.summary$Cycle, function(foo) sum(foo$Count), 
+               simplify = TRUE)
+    statdf.summary <- data.frame(Cycle = as.integer(rownames(means)), 
+                                 Mean = means, Q25 = as.vector(q25s), Q50 = as.vector(q50s), 
+                                 Q75 = as.vector(q75s), Cum = 10 * as.vector(cums)/rc)
+    
+    }
+  return(statdf.summary)
 }
