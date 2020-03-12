f9d00d89f2dd9126834c1987396bb2b01b7f9a1f
cicese <cigom@ciceses-MacBook-Pro.local>
2020-03-12 15:16:49 -0700

    combine features

--- combine_features.R
+++ combine_features.R
@@ -1,67 +1,146 @@
 
-# Ricardo Gomez-Reyes
+# Ricardo Gomez-Reyes, March, 2020
+# 1. 
+# 2. Detect low-abundance (laf) features ----
+# 3. Clean non-animal taxonomy ----
+# 4. Filter laf and out file -----
+
+rm(list=ls())
 
 # 0. Load functions ----
 require(data.table)
 
-path <- "/Users/cigom/metagenomics/18S_MULTIRUN_XIXIMIS/downstream"
-
-setwd(path)
-
 url <- 'https://raw.githubusercontent.com/RJEGR/metagenomics/master/readtx.R'
 
 source(url)
-# rank ----
-silva <- TRUE
 
-if(silva) {
-  # Silva Ranks
-  Ranks <- c("Reino", "Filo", "Clase", "Orden", 
-              "Familia", "Genero", "Especie")
-} else {
-  # BOLD ranking 
-  Ranks <- c("root","Domain","Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
+# set rank names ----
+
+
+# la intension es tener un formato de rankins segun la base de datos.
+# y hacer un check dbformat para en base de ello accesar al formato correcto.
+
+dbformat <- c("silva", "bold", 'midori')
+dbformat[dbformat %in% db]
+
+frmt <- TRUE
+dbformat = "bold"
+
+# Silva ranking format
+Ranks <- list(Reino=sym("Rank_1"), Filo=sym("Rank_2"), 
+                Clase=sym("Rank_3"), Orden=sym("Rank_4"), 
+                Familia=sym("Rank_5"), Genero=sym("Rank_6"),
+                Especie=sym("Rank_7"))
+
+# BOLD ranking format
+Ranks <- list(root = sym("Rank_1"), Dominio=sym("Rank_2"), 
+                Reino=sym("Rank_3"), Filo=sym("Rank_4"), 
+             Clase=sym("Rank_5"), Orden=sym("Rank_6"), 
+             Familia=sym("Rank_7"), Genero=sym("Rank_8"),
+             Especie=sym("Rank_9"))
+  
 }
 
-# 1. Read and combine data ----
+# Funcions ----
+combine_features <- function(count_tbl, tax_tbl, k = 1, tax_header = TRUE) {
+  
+  require(data.table)
+  require(dplyr)
+  
+  # 1. Read and combine data ----
+  
+  features <- data.table(read.table(count_tbl))
+  tax <- data.table(read_rdp(tax_tbl, header = tax_header))
+  size <- data.table(fsize = rowSums(features))
+  nsams <- data.frame(nsams = c(apply(features, 1, function(x) {sum(x > 0)})))
+  
+  # test <- identical(rownames(features), rownames(tax))
+  # 
+  # if(test) { combine <- cbind(features, tax, size) }
+  # else { combine <- features }
+  
+  combine <- cbind(features, tax, size, nsams)
+  
+  # 2. Detect low-abundance (laf) features ----
+  
+  combine[, ksize := ifelse(fsize > k,'Keep','Drop')]
+  
+  # 3.Clean non-animal taxonomy ----
+  
+  groups <- table(tax$Rank_1)
+  
+  g <- groups[groups == max(groups)]
+  
+  others <- combine[combine$Rank_1 != names(g),]
+  others <- rename(others, !!!Ranks)
+  
+  combine <- combine[combine$Rank_1 == names(g),]
+  
+  nrow(combine) == g
+  
+  # 4. Filter laf and out file ---- 
+  sam <- colnames(features)
+  
+  out <- filter(combine, ksize == 'Keep')
+  
+  out <- select(out, all_of(sam), !!!Ranks)
+  
+  
+  # write.table(out, file = 'combine_tax_count_format.txt')
+  
+  return(list("clean"=out, "complete"=combine, "others" = others))
+  
+}
 
-f <- list.files(pattern = "shared", full.names = TRUE)
-t <- list.files(pattern = "taxonomy", full.names = TRUE)
+# SILVA ----
+# Silva ranking format
+Ranks <- list(Reino=sym("Rank_1"), Filo=sym("Rank_2"), 
+              Clase=sym("Rank_3"), Orden=sym("Rank_4"), 
+              Familia=sym("Rank_5"), Genero=sym("Rank_6"),
+              Especie=sym("Rank_7"))
 
-features <- data.table(read.table(f))
-tax <- data.table(read_rdp(t, header = TRUE))
+path <- "/Users/cigom/metagenomics/18S_MULTIRUN_XIXIMIS/downstream"
+f <- list.files(pattern = "shared", full.names = TRUE, path = path)
+t <- list.files(pattern = "taxonomy", full.names = TRUE, path = path)
 
-size <- data.table(fsize = rowSums(features))
-test <- identical(rownames(features), rownames(tax))
+out_silva <- combine_features(f, t, tax_header = TRUE)
+
+write.table(out_silva$clean, file = paste0(path,'/','combine_tax_count_format1.txt' ))
+
+# BOLD ----
+path <- "/Users/cigom/metagenomics/COI/MULTIRUN/multirun_20190829_AMB_XIXIM/"
+
+count_tbl <- list.files(pattern = "count.table", full.names = TRUE, path = path)
+
+tax_tbl <- list.files(pattern = "taxonomy", full.names = TRUE, path = path)
 
 
-if(test) { combine <- cbind(features, tax, size) } else { combine <- features }
+out_bold <- combine_features(count_tbl, tax_tbl, tax_header = FALSE)
 
-# 2.Clean non-animal taxonomy ----
+write.table(out_bold, file = paste0(path,'/','combine_tax_count_format2.txt' ))
 
-groups <- table(tax$Rank_1)
 
-g <- groups[groups == max(groups)]
 
-others <- combine[combine$Rank_1 != names(g),]
 
-combine <- combine[combine$Rank_1 == names(g),]
 
-nrow(combine) == g
 
-# 3. Check low-abundance (laf) features ----
 
-k <- 1
+# para dar continuidad al combine, estaria bien meter algunas visualizaciones. etc.
+others <- data.table(out_silva$others)
 
-combine[, ksize := ifelse(fsize > k,'Keep','Drop')]
+table(others$Resolution)
 
-# x. agglomerate ??
+aglom_ab <- function(x, rank) {
+  x <- as.data.frame(x)
+  tax_g <- aggregate(x[,'fsize'],  by = list(x[, rank]), FUN = sum)
+  n_asvs <- aggregate(x[, rank],  by = list(x[, rank]), FUN = length)
+  
+  if(identical(tax_g[,1], n_asvs[,1])){
+      tax_out <- data.frame(lineage = tax_g[,1], 
+                            Abundance = tax_g[,2], 
+                            Size = n_asvs[,2])
+  }
+  return(tax_out)
+}
 
-# 4. Filter laf and out file 
-
-sam <- c(colnames(features), Ranks)
-
-out <- filter(combine, ksize == 'Keep')
-out <- select(combine, all_of(sam))
-
-write.table(out, file = 'combine_tax_count_format.txt')
+aglom_ab(others, rank = 'Reino')
